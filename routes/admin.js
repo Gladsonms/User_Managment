b9555d1e2c84405fbf7d99c803c2cc22da588c0a
Gladsonms <gladsonsaji4@gmail.com>
2021-09-19 12:46:33 +0530

    added admin home

--- routes/admin.js
+++ routes/admin.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('admin')
 });
+router.get("/home",function(req,res,next){
 
+res.render("adminHome")
+});
 module.exports = router;
